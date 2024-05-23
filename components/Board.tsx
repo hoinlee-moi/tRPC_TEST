f0df008b09c51e4aa3826853a9512862e5beeb6b
hoinlee-moi <lhi3048@gmail.com>
2024-05-23 15:29:47 +0900

    error fix

--- components/Board.tsx
+++ components/Board.tsx
@@ -7,14 +7,13 @@ export default function Board() {
 
   return (
     <div>
-      {posts?.map(({ title, createdAt, content }) => (
+      {posts?.map(({ title, content }) => (
         <div
-          key={createdAt}
+          key={Math.random()}
           className="flex flex-col border border-solid border-blue-400 mb-3 p-2"
         >
           <h1 className="font-bold text-2xl mb-2">Title : {title}</h1>
           <p className="mb-4">{content}</p>
-          <p className="text-slate-400">{createdAt}</p>
         </div>
       ))}
     </div>
